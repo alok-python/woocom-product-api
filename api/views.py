327f37f234d06e3bf629c0ac19d6bc442f5e199e
alok-python <alokkumar199907@gmail.com>
2022-09-02 21:11:06 +0530

    first commit

--- api/views.py
+++ api/views.py
@@ -1,14 +1,9 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from api.models import Productmodel
 from api.serializer import ProductSerializer
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 # Create your views here.
 
 
-def apidocumentation(request):
-    return render(request,'index.html')
 
 
 class ProductViewSet(viewsets.ModelViewSet):
